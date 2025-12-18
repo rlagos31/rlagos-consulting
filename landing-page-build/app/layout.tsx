3c87461ebba7beb49a931970e6dff8987210baf2
rlagos31 <ronald.l.lagos31@gmail.com>
2025-12-18 01:22:08 -0500

    Update layout.tsx

--- landing-page-build/app/layout.tsx
+++ landing-page-build/app/layout.tsx
@@ -14,19 +14,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/rlagosICON.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/rlagosICON.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/rlagosICON.png",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/rlagosICON.png",
   },
 }
 
